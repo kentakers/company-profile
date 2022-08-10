08bcec6a5ddc419467ed29ac16904820dd290d13
Aditya Tariqh <aditya.tariqh@gmail.com>
2022-08-10 18:43:12 +0700

    Create Module Vision, Mission & Value

--- Company-Profile/about/views.py
+++ Company-Profile/about/views.py
@@ -1,9 +1,22 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
     context = {
-        'title': 'Onasis Indonesia',
-        'heading': 'Selamat Datang di About',
+        'title': 'About Us',
+        'heading': 'Selamat Datang di About Us',
+        'app_css': 'about/css/styles.css',
     }
     return render(request, 'about/index.html', context)
+
+
+def visionMission(request):
+    context = {
+        'title': 'Vision, Mission & Value',
+        'heading': 'Vision, Mission & Value PT. Onasis Indonesia',
+        'app_css': 'about/css/styles.css',
+    }
+    return render(request, 'about/vision-mission.html', context)
